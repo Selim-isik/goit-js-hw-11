34b612d7e6f378e1c2cecbf0445fe8386e7d65ca
Selim Işık <selimoyunda6@gmail.com>
2025-07-12 16:01:48 +0300

    try

--- vite.config.js
+++ vite.config.js
@@ -1,56 +1,48 @@
 import { defineConfig } from 'vite';
 import { glob } from 'glob';
-import path from 'path';
 import injectHTML from 'vite-plugin-html-inject';
 import FullReload from 'vite-plugin-full-reload';
 import SortCss from 'postcss-sort-media-queries';
 
-export default defineConfig(({ command }) => ({
-  define: {
-    [command === 'serve' ? 'global' : '_global']: {},
-  },
-  root: 'src',
-  publicDir: '../public',
-  server: {
-    port: 3000,
-    hot: true,
-  },
-  css: {
-    postcss: {
-      plugins: [SortCss({ sort: 'mobile-first' })],
+export default defineConfig(({ command }) => {
+  return {
+    define: {
+      [command === 'serve' ? 'global' : '_global']: {},
     },
-  },
-  build: {
-    sourcemap: command === 'serve',
-    rollupOptions: {
-      input: Object.fromEntries(
-        glob
-          .sync('./src/**/*.html')
-          .map(file => [path.basename(file, '.html'), path.resolve(file)])
-      ),
-      output: {
-        manualChunks(id) {
-          if (id.includes('node_modules')) {
-            return 'vendor';
-          }
+    root: 'src',
+    build: {
+      sourcemap: true,
+      rollupOptions: {
+        input: glob.sync('./src/*.html'),
+        output: {
+          manualChunks(id) {
+            if (id.includes('node_modules')) {
+              return 'vendor';
+            }
+          },
+          entryFileNames: chunkInfo => {
+            if (chunkInfo.name === 'commonHelpers') {
+              return 'commonHelpers.js';
+            }
+            return '[name].js';
+          },
+          assetFileNames: assetInfo => {
+            if (assetInfo.name && assetInfo.name.endsWith('.html')) {
+              return '[name].[ext]';
+            }
+            return 'assets/[name]-[hash][extname]';
+          },
         },
-        assetFileNames: ({ name }) => {
-          const ext = name?.split('.').pop();
-          if (ext === 'css') return 'assets/css/[name]-[hash][extname]';
-          if (ext === 'html') return '[name].[ext]';
-          return 'assets/[name]-[hash][extname]';
-        },
-        entryFileNames: 'assets/js/[name]-[hash].js',
       },
+      outDir: '../dist',
+      emptyOutDir: true,
     },
-    outDir: '../dist',
-    emptyOutDir: true,
-    chunkSizeWarningLimit: 1600,
-  },
-  plugins: [injectHTML(), FullReload(['./src/**/*.html', './src/**/*.scss'])],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src'),
-    },
-  },
-}));
+    plugins: [
+      injectHTML(),
+      FullReload(['./src/**/**.html']),
+      SortCss({
+        sort: 'mobile-first',
+      }),
+    ],
+  };
+});
